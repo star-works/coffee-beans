d9e0116dbf4da433bd53fd2d2507f9d1864e11aa
Arun <arunkumar9034566237@gmail.com>
2024-03-04 22:04:36 +0530

    code modified

--- src/components/ui/carousel.jsx
+++ src/components/ui/carousel.jsx
@@ -1,10 +1,11 @@
+"use client";
 import * as React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { NextArrow, PrevArrow } from "../Icons";
+import { NextArrow, PrevArrow } from "../common/Icons";
 
 const CarouselContext = React.createContext(null);
 
