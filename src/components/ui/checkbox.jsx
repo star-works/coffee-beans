6c9909351172de97db9c0c8bb5b5ca2b39cc6e08
Arun <arunkumar9034566237@gmail.com>
2024-03-06 19:25:59 +0530

    radio btn

--- src/components/ui/checkbox.jsx
+++ src/components/ui/checkbox.jsx
@@ -1,7 +1,6 @@
 "use client";
-import * as React from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { Check } from "lucide-react";
+import * as React from "react";
 
 import { cn } from "@/lib/utils";
 import { InputCheckbox } from "../common/Icons";
--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,5 +1,6 @@
 "use client";
 import * as React from "react";
+
 import { cn } from "@/lib/utils";
 
 const Input = React.forwardRef(({ className, type, ...props }, ref) => {
