49928cb789986e3520b79f5fffa1acdcfadff580
Neha <neharadialcode@gmail.com>
2024-03-08 20:44:30 +0530

    category

--- src/app/category/page.js
+++ src/app/category/page.js
@@ -5,8 +5,8 @@ import { Button } from "@/components/ui/button";
 
 const page = () => {
   return (
-    <div className="pt-[45px] md:pb-20 pb-[45px] max-w-[1120px] mx-auto xl:px-0 px-3">
-      <div className="flex gap-6 sm:items-center sm:flex-row flex-col mb-10">
+    <div className="pt-[45px] md:pb-20 pb-[45px] max-w-[1120px] mx-auto xl:px-0 px-3 ">
+      <div className="flex gap-6 sm:items-center sm:flex-row flex-col mb-10 pt-20 sm:mt-12 ">
         <h2 className="font-semibold text-[32px] leading-[38.4px] text-[#090909]">
           Category Page
         </h2>
@@ -25,7 +25,7 @@ const page = () => {
           </Button>
         </div>
       </div>
-      <div className="flex flex-col sm:flex-row gap-4 lg:gap-10">
+      <div className="flex flex-col sm:flex-row gap-4 lg:gap-10 pt-3">
         <AccordionComponent /> <CategoryCard />
       </div>
     </div>
