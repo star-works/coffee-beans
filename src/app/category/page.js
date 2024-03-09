0ee7eb40cc099683bd88b105500773b3833977b0
Neha <neharadialcode@gmail.com>
2024-03-09 15:33:07 +0530

    error removed

--- src/app/category/page.js
+++ src/app/category/page.js
@@ -3,15 +3,6 @@ import PageComponent from "@/components/category/PageComponents";
 import { useEffect, useState } from "react";
 
 const page = () => {
-  const [category, setCategory] = useState("");
-  const [selectedCategory, setSelectedCategory] = useState([]);
-  const selectCategoryFilter = () => {
-    setSelectedCategory([...selectedCategory, category]);
-  };
-
-  useEffect(() => {
-    selectCategoryFilter();
-  }, [category]);
   return <PageComponent />;
 };
 
