ccf6177ff77f5f679fa9c58bc365ca037c127b42
Neha <neharadialcode@gmail.com>
2024-03-12 14:36:46 +0530

    accordion items seprated

--- src/components/category/Accordion.jsx
+++ src/components/category/Accordion.jsx
@@ -1,12 +1,28 @@
 "use client";
 import { Accordion } from "@/components/ui/accordion";
 import Roastlevel from "./Roastlevel";
+import CoffeeNotes from "./CoffeeNotes";
+import BeanSort from "./BeanSort";
+import BeanOrigin from "./BeanOrigin";
+import Altitude from "./Altitude";
+import CoffeeType from "./Coffeetype";
+import RoasterLocation from "./RoasterLocation";
+import Certifications from "./Certifications";
+import Subscriptions from "./Subscription";
 
 const AccordionComponent = () => {
   return (
     <div className="lg:max-w-[400px] md:max-w-[300px] w-full">
       <Accordion type="single" collapsible className="w-full">
         <Roastlevel />
+        <CoffeeNotes />
+        <BeanSort />
+        <BeanOrigin />
+        <Altitude />
+        <CoffeeType />
+        <RoasterLocation />
+        <Certifications />
+        <Subscriptions />
       </Accordion>
     </div>
   );
---
+++ src/components/category/CoffeeType.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { RadioInputs } from "./AllInput";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { BlackSearch } from "../common/Icons";
+const CoffeeType = () => {
+  const options = ["Light", "Medium", "Dark"];
+  const [search, setSerach] = useState(options);
+  const searchHandler = (e) => {
+    const newArr = options.filter((obj) =>
+      obj.toLocaleLowerCase().includes(e.toLocaleLowerCase())
+    );
+    setSerach(newArr);
+  };
+  return (
+    <AccordionItem value={`1`} defaultOpen={`1`} className="mb-6  border-b-0">
+      <AccordionTrigger className="ps-6 pe-5 py-4 bg-[#f9fafa] rounded-2xl hover:no-underline font-medium text-lg text-[#090909]">
+        Roast level
+      </AccordionTrigger>
+      <AccordionContent className="pt-5">
+        <div className={`relative mb-[25px]`}>
+          <Label htmlFor="icon">
+            <span className="opacity-40 absolute right-4 top-[50%] -translate-y-[50%]">
+              <BlackSearch />
+            </span>
+          </Label>
+          <Input
+            onChange={(e) => searchHandler(e.target.value)}
+            id="icon"
+            placeholder="Search Roast level..."
+            className="border-0 pb-4 px-4 border-b rounded-none focus-visible:outline-none focus-visible:ring-0"
+          />
+        </div>
+        <div className="px-4">
+          <RadioInputs search={search} />
+        </div>
+      </AccordionContent>
+    </AccordionItem>
+  );
+};
+
+export default CoffeeType;
