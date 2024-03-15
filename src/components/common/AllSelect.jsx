721e7348da15691cc5012cb82370d126c5971527
Arun <arunkumar9034566237@gmail.com>
2024-03-15 22:00:48 +0530

    select

--- src/components/common/AllSelect.jsx
+++ src/components/common/AllSelect.jsx
@@ -10,32 +10,6 @@ import {
 } from "@/components/ui/select";
 import { CategoryIcon } from "./Icons";
 
-export const CategorySelect = (props) => {
-  return (
-    <>
-      <Select className="p-0 border-none focus:border-none ">
-        <SelectTrigger
-          className={`p-0 bg-transparent group min-w-[97px] text-white hover:text-opacity-80 duration-300 border-none outline-none focus:outline-none`}
-        >
-          <div className={`hidden`}>
-            <CategoryIcon />
-          </div>
-          <SelectValue className="p-0" placeholder="Categories" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Fruits</SelectLabel>
-            <SelectItem value="apple">Apple</SelectItem>
-            <SelectItem value="banana">Banana</SelectItem>
-            <SelectItem value="blueberry">Blueberry</SelectItem>
-            <SelectItem value="grapes">Grapes</SelectItem>
-            <SelectItem value="pineapple">Pineapple</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-    </>
-  );
-};
 export const LocationSelect = (props) => {
   return (
     <>
@@ -49,7 +23,7 @@ export const LocationSelect = (props) => {
           <SelectValue className="p-0" placeholder="Location" />
         </SelectTrigger>
         <SelectContent>
-          <SelectGroup>
+          <SelectGroup className="flex flex-wrap p-3">
             <SelectLabel className="text-[14px]">Location</SelectLabel>
             <SelectItem value="India">India</SelectItem>
             <SelectItem value="Australia">Australia</SelectItem>
