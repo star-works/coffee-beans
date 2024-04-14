46ce91855ed7461b926bd76574b1d79e01ceaf33
Arun <arunkumar9034566237@gmail.com>
2024-04-14 21:59:40 +0530

    sidebar links

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -102,11 +102,21 @@ const Sidebar = ({ showNav, setShowNav }) => {
               </p>
             </div>
             <Link href="/dashboard/settings">
-              <div className="px-2 py-2 rounded-[8px] hover:bg-[#F9FAFB] transition-all duration-300 flex items-center gap-2 group cursor-pointer">
-                <span>
+              <div
+                className={`px-2 py-2 rounded-[8px] hover:bg-[#F9FAFB] transition-all duration-300 flex items-center gap-2 group cursor-pointer ${
+                  router === "/dashboard/settings"
+                    ? "bg-[#F9FAFB] px-2 py-2  rounded-[8px] "
+                    : ""
+                }`}
+              >
+                <span >
                   <SettingsIcon />
                 </span>
-                <p className="text-sm -tracking-[3%] text-[#4B5563] font-semibold group-hover:text-[#D3756B] transition-all duration-300">
+                <p
+                  className={`text-sm -tracking-[3%] text-[#4B5563] font-semibold group-hover:text-[#D3756B] transition-all duration-300 ${
+                    router === "/dashboard/settings" && "text-[#D3756B]"
+                  }`}
+                >
                   Settings
                 </p>
               </div>
